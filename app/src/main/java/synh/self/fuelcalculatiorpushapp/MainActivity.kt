27557d92edf03e4c0077689c571eaa51e9395f16
NightOfHorror <nightbringer96@yandex.ru>
2019-06-14 15:26:24 +0300

    Added auto calculation

--- app/src/main/java/synh/self/fuelcalculatiorpushapp/MainActivity.kt
+++ app/src/main/java/synh/self/fuelcalculatiorpushapp/MainActivity.kt
@@ -3,7 +3,8 @@ package synh.self.fuelcalculatiorpushapp
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -26,6 +27,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewInit()
         buttonsInit()
+        calcInit()
+    }
+
+    private fun calcInit() {
+        rangeView.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                calcFuel()
+            }
+        })
+
+        consView.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                calcFuel()
+            }
+        })
+
+        costView.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                calcFuel()
+            }
+        })
     }
 
     @SuppressLint("SetTextI18n")
@@ -34,15 +62,21 @@ class MainActivity : AppCompatActivity() {
             val rangeStr = rangeView.text.toString()
             val consStr = consView.text.toString()
             val costStr = costView.text.toString()
-            if (rangeStr.isEmpty() || consStr.isEmpty() || costStr.isEmpty()) {
-                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
-            } else {
+
+            var isValid = true
+            if (rangeStr.isEmpty() || consStr.isEmpty() || costStr.isEmpty()){
+                isValid = false
+                //Можно дописать визуальное отображение ошибок
+            }
+            if (isValid){
                 val range = rangeStr.toDouble()
                 val cons = consStr.toDouble()
                 val cost = costStr.toDouble()
                 val result = range / 100 * cons
                 resultView.text = result.toString()
                 moneyView.text = "(" + (result * cost).toString() + "\u20BD)"
+            } else{
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
             }
         }
 
@@ -64,5 +98,27 @@ class MainActivity : AppCompatActivity() {
         costView = findViewById(R.id.edit_litre_cost)
         calcButton = findViewById(R.id.button_calc)
         clearButton = findViewById(R.id.button_clear)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calcFuel(){
+        val rangeStr = rangeView.text.toString()
+        val consStr = consView.text.toString()
+        val costStr = costView.text.toString()
+
+        var isValid = true
+        if (rangeStr.isEmpty() || consStr.isEmpty() || costStr.isEmpty()){
+            isValid = false
+            //Можно дописать визуальное отображение ошибок
+        }
+
+        if (isValid){
+            val range = rangeStr.toDouble()
+            val cons = consStr.toDouble()
+            val cost = costStr.toDouble()
+            val result = range / 100 * cons
+            resultView.text = result.toString()
+            moneyView.text = "(" + (result * cost).toString() + "\u20BD)"
+        }
     }
 }
